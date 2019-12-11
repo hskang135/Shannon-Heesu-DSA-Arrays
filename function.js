d9a89cde4494610003b4882913386c2f927706e9
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-12-11 15:31:01 -0700

    fixed filtering function

--- function.js
+++ function.js
@@ -111,8 +111,25 @@ function filtering(array) {
   return array;
 }
 
-// console.log(filtering([-20, -1, 1, 1, 2, 2, 3, 4, 5, 6, 7]));
-// the big O of the above algorithm is O(n) linear.
+// console.log(filtering([-1, 1, 1, 2, 2, 3, 4, 5, 6, 7]));
+// the big O of the above algorithm is O(n^2) polynomial because we loop through 
+// the array based on the length, and then again when we use the splice method
+//we iterate over the array every time it is called to copy the elements into each previous index.
+
+//alternate solution
+function filteringTwo(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 5) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+// console.log(filteringTwo([-1, 1, 1, 2, 2, 3, 4, 5, 6, 7]));
+//this solution would be big O(n) linear based on the size of the array. It iterates over 
+// the array once in a for loop. 
+
 
 function maxSum(array) {
   let max = array[0];
@@ -159,10 +176,10 @@ function removeChar(string, chars) {
 function products(arr) {
   let result = 1;
   let total = [];
-  for(let i=0; i<arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     result = result * arr[i]
   }
-  for(let j=0; j<arr.length; j++) {
+  for (let j = 0; j < arr.length; j++) {
     total.push(result / arr[j])
   }
   return total;
@@ -173,18 +190,18 @@ function products(arr) {
 
 function twoD(arr) {
   let newarr = [];
-  for(let x=0; x<arr.length; x++) {
+  for (let x = 0; x < arr.length; x++) {
     newarr.push([...arr[x]])
   }
   // x = row
-  for(let x=0; x<arr.length; x++) {
+  for (let x = 0; x < arr.length; x++) {
     // y = column
-    for(let y=0; y<arr[x].length; y++) {
-      if(arr[x][y] === 0) {
-        for(let z=0; z<arr[x].length; z++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      if (arr[x][y] === 0) {
+        for (let z = 0; z < arr[x].length; z++) {
           newarr[x][z] = 0;
         }
-        for(let m=0; m<arr.length; m++) {
+        for (let m = 0; m < arr.length; m++) {
           newarr[m][y] = 0;
         }
       }
@@ -204,9 +221,9 @@ function twoD(arr) {
 // the big O of the above algorithm is o(n^4) polynomial.
 
 function rotation(str1, str2) {
-  for(let i=0; i<str1.length; i++) {
-    let newstr = str2[i] + str2.slice(i+1) + str2.slice(0, i);
-    if(newstr === str1) {
+  for (let i = 0; i < str1.length; i++) {
+    let newstr = str2[i] + str2.slice(i + 1) + str2.slice(0, i);
+    if (newstr === str1) {
       return true;
     }
   }
